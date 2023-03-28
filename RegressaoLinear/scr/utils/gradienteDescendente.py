6e90ba1a7f83e75199561cf1355d98df035689ea
VitorSantana03 <vitor.santana@neon.com.br>
2023-03-28 10:21:41 -0300

    Adicionando a função de equação normal

--- RegressaoLinear/scr/utils/gradienteDescendente.py
+++ RegressaoLinear/scr/utils/gradienteDescendente.py
@@ -46,18 +46,32 @@ def atualizaThetas(derivadasParciais,thetas,taxaAprendizado):
         thetasAtualizados.append(theta)
     return thetasAtualizados
 
+def erroQuadradoMedio(Ypred,target):
+    return ((np.sum(((Ypred-target)**2)))/(2*len(target)))
 
 def gradienteDescendente(features,target,thetas,numIter=100,taxaAprendizado=0.01):
-    erroQuadradoMedio = []
+    erro = []
     
     for iteracao in range(numIter):
         Ypred = equacaoRegressaoLinear(thetas,features)
         derivadasParcial = derivadasParciais(thetas,features,target,Ypred)
         thetas = atualizaThetas(derivadasParcial,thetas,taxaAprendizado)
-        erroQuadradoMedio.append((np.sum(((Ypred-target)**2)))/(len(features)))
+        erro.append(erroQuadradoMedio(Ypred,target))
     
-    return thetas,erroQuadradoMedio
+    return thetas,erro
 
+def equacaoNormal(features,target):
+    array_ums = np.ones(len(features),int)
+    X = np.matrix([array_ums,features['TamanhoCasa'],features['NumeroQuartos']]).transpose()
+    Xtransposta = X.transpose()
+    y = np.matrix([target['PrecoCasa']]).transpose()
+
+    thetas = np.linalg.inv(Xtransposta*X)*Xtransposta*y
+    thetas = [float(thetas[0]), float(thetas[1]), float(thetas[2])]
+    Ypred = equacaoRegressaoLinear(thetas,features)
+    return thetas, erroQuadradoMedio(Ypred,target['PrecoCasa'])
+    
+    
 def plot_3d(features,target, thetas, taxa):
     fig = plt.figure(figsize= (16,9))
     ax = plt.axes(projection = '3d')
